42a1caf51f6d25c19ccbe508c41b46007ee2f49f
thatGui <gui@thatgui.com>
2023-08-22 14:47:50 +0100

    trying to fix email bug

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -5,6 +5,7 @@ import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import GithubProvider from 'next-auth/providers/github';
 
 export const authOptions: NextAuthOptions = {
+	secret: process.env.NEXTAUTH_SECRET,
 	adapter: PrismaAdapter(prisma),
 	providers: [
 		GithubProvider({
